7e407106827d4da3a14692c0f55db9c3097e0eb2
Raimund Ege <ege@niu.edu>
2025-10-18 18:12:26 -0500

    narrowed  mariadb privileges

--- blitzdb/mysql.sql
+++ blitzdb/mysql.sql
@@ -3,9 +3,13 @@
 --
 
 CREATE USER 'student'@'%' IDENTIFIED BY 'student';
-CREATE USER 'processor'@'172.18.0.%' IDENTIFIED BY 'processor';
-GRANT ALL PRIVILEGES ON *.* TO `processor`@`172.18.0.%`;
-GRANT USAGE ON *.* TO `student`@`%`;
 GRANT SELECT ON `csci467`.* TO `student`@`%`;
+CREATE USER 'instructor'@`localhost` IDENTIFIED BY 'instructor';
+GRANT ALL PRIVILEGES ON Payroll.* TO `instructor`@`localhost`;
+CREATE USER 'instructor'@`10.158.56.43` IDENTIFIED BY 'instructor';
+GRANT ALL PRIVILEGES ON Encrypt.* TO `instructor`@`10.158.56.43`;
+CREATE USER 'processor'@`172.18.0.%` IDENTIFIED BY 'processor';
+GRANT ALL PRIVILEGES ON csci467.* TO `processor`@`172.18.0.%`;
+
 
 FLUSH PRIVILEGES;
