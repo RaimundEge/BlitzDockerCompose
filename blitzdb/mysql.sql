e1f6dec5ab8e7a92cb936b91a0f0de66dfb1fd80
Raimund Ege <ege@niu.edu>
2025-10-18 18:44:12 -0500

    fixed Payroll web app

--- blitzdb/mysql.sql
+++ blitzdb/mysql.sql
@@ -4,12 +4,11 @@
 
 CREATE USER 'student'@'%' IDENTIFIED BY 'student';
 GRANT SELECT ON `csci467`.* TO `student`@`%`;
-CREATE USER 'instructor'@`localhost` IDENTIFIED BY 'instructor';
-GRANT ALL PRIVILEGES ON Payroll.* TO `instructor`@`localhost`;
 CREATE USER 'instructor'@`10.158.56.43` IDENTIFIED BY 'instructor';
 GRANT ALL PRIVILEGES ON Encrypt.* TO `instructor`@`10.158.56.43`;
 CREATE USER 'processor'@`172.18.0.%` IDENTIFIED BY 'processor';
 GRANT ALL PRIVILEGES ON csci467.* TO `processor`@`172.18.0.%`;
-
+CREATE USER 'demo'@`172.18.0.1` IDENTIFIED BY 'demo';
+GRANT ALL PRIVILEGES ON Payroll.* TO `demo`@`172.18.0.1`;
 
 FLUSH PRIVILEGES;
