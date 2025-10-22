a5437e0d0f2618fb6997bcdf9b6771fdf09bc85d
Raimund Ege <ege@niu.edu>
2025-10-22 14:03:28 -0500

    added require ssl for student

--- blitzdb/mysql.sql
+++ blitzdb/mysql.sql
@@ -4,6 +4,9 @@
 
 CREATE USER 'student'@'%' IDENTIFIED BY 'student';
 GRANT SELECT ON `csci467`.* TO `student`@`%`;
+ALTER USER 'student'@'%' REQUIRE SSL;
+CREATE USER 'student'@'10.158.%.%' IDENTIFIED BY 'student';
+GRANT SELECT ON `csci467`.* TO `student`@`10.158.%.%`;
 CREATE USER 'instructor'@`10.158.56.43` IDENTIFIED BY 'instructor';
 GRANT ALL PRIVILEGES ON Encrypt.* TO `instructor`@`10.158.56.43`;
 CREATE USER 'processor'@`172.18.0.%` IDENTIFIED BY 'processor';
